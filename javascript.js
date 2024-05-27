b1ac0a88f58f7c3fe14e11acf49cb909a9c6ff09
hutleangchhun <152780006+hutleangchhun@users.noreply.github.com>
2024-05-27 16:18:19 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function play(){
+	const car = document.getElementById("car");
+	car.style.animationPlayState = "running";
+}
+function pause(){
+	const car = document.getElementById("car");
+	car.style.animationPlayState = "paused"
+}
